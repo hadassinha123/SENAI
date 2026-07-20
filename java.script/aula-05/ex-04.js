139db8c0561a3fcda2a1d941849d826a3a6fee33
HADASSA ALVES <hadassa_alves@estudante.sesisenai.org.br>
2026-07-20 17:07:17 -0300

    teste

---
+++ java.script/aula-05/ex-04.js
@@ -0,0 +1,26 @@
+let diasemana = 7
+
+switch (diasemana) {
+    caso 1 :
+    console.log("Domingo")
+    break;
+    caso 2:
+    console.log("Segundo")
+    break;
+    caso 3:
+    console.log("terça")
+    break;
+    caso 4:
+    console.log("Quarta")
+    break;
+     caso 5:
+    console.log("Quinta")
+    break;
+     caso 6:
+    console.log("sexta")
+    break;
+     caso 7:
+    console.log("sabado")
+    break;
+
+}
